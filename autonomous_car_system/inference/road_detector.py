90fd60ba8a7f17495a1e9fef04d97b64bf6a8a9d
Nhat Minh <pnnhatminh2005@gmail.com>
2026-07-16 00:01:50 +0700

    fix color lut

--- autonomous_car_system/inference/road_detector.py
+++ autonomous_car_system/inference/road_detector.py
@@ -15,9 +15,10 @@ _MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
 _STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
 
 COLOR_LUT = np.array([
-    [0, 0, 0],        # background
+    [0, 0, 0],        # _background_
     [0, 200, 0],      # road (green)
     [200, 200, 0],    # lane_marking (yellow)
+    [0, 0, 200],      # dividing_line (red)
 ], dtype=np.uint8)
 
 
